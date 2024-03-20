58334608a594da02b0d059e0f445a2a1225972a5
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-03-20 22:18:23 +0100

    javascript page done

--- dist/js/javascript.js
+++ dist/js/javascript.js
@@ -17,25 +17,47 @@ function fizzBuzz(input) {
 
   for (var i = 1; i <= input; i++) {
 
+    console.log("i is " + i);
     if (i == 0) {
-      //fizzBuzzText = `1 `;
+      
       fizzBuzzArray.push(`1, `);
     }
     if (i % 3 == 0 && i % 5 == 0) {
 
-      //fizzBuzzText += ` <span class="inline-teal">fizzBuzz</span>`;
-      fizzBuzzArray.push(` <span class="inline-teal inline-bold">fizzBuzz</span>, `);
+      if (i == input) {
+
+        fizzBuzzArray.push(` <span class="inline-teal inline-bold">fizzBuzz</span>`);
+      } else {
+
+        fizzBuzzArray.push(` <span class="inline-teal inline-bold">fizzBuzz</span>, `);
+      }
     } else if (i % 3 == 0) {
 
-      //fizzBuzzText += ` <span class="inline-yellow">fizz</span> `;
-      fizzBuzzArray.push(` <span class="inline-yellow">fizz</span>, `);
+      if (i == input) {
+        fizzBuzzArray.push(` <span class="inline-yellow">fizz</span>`);
+      } else {
+        fizzBuzzArray.push(` <span class="inline-yellow">fizz</span>, `);
+      }
+      
     } else if (i % 5 == 0) {
 
-      //fizzBuzzText += ` <span class="inline-coral">buzz</span> `;
-      fizzBuzzArray.push(` <span class="inline-coral">buzz</span>, `);
+      if (i == input) {
+
+        fizzBuzzArray.push(` <span class="inline-coral">buzz</span>`);  
+      } else {
+
+        fizzBuzzArray.push(` <span class="inline-coral">buzz</span>, `);
+      }
     } else {
-      //fizzBuzzText += ` ${i} `;
-      fizzBuzzArray.push(` ${i}, `);
+
+      if (i == input) {
+
+        console.log("i - 1");
+        fizzBuzzArray.push(` ${i}`);
+      } else {
+
+        fizzBuzzArray.push(` ${i}, `);
+      }
     }
   }
 
@@ -82,7 +104,7 @@ function fibonachiGenerator(input) {
       console.log(nextNum);
       if (i == input - 1) {
 
-        fiboSeq.push(` <span class="inline-coral inline-bold">${nextNum}</span>, `);
+        fiboSeq.push(` <span class="inline-coral inline-bold">${nextNum}</span> `);
       } else {
 
         fiboSeq.push(` ${nextNum}, `);
@@ -99,11 +121,59 @@ function fibonachiGenerator(input) {
   })
 }
 
+// Footer Page Specs
 const jsPortfolioButton = document.getElementById('tm-javascript-portfolio-specs-button');
-const homeSpecs = document.getElementById('tm-home-specs');
-const homeSpecsDivs = document.querySelectorAll('#tm-home-specs div');
+const jsPortfolioSpecs = document.getElementById('tm-javascript-portfolio-specs');
+const jsPortfolioSpecsDivs = document.querySelectorAll('#tm-javascript-portfolio-specs div');
 
 jsPortfolioButton.addEventListener('click', () => {
 
+  if (!jsPortfolioButton.classList.contains('active')) {
+    jsPortfolioButton.classList.add('active');
+    jsPortfolioButton.innerHTML = "Remove Page Specs";
+    jsPortfolioSpecs.classList.add('active');
+    
+    jsPortfolioSpecsDivs.forEach((div, index) => {
 
+      setTimeout(() => {
+
+        div.classList.add('active');
+      }, index * 420);
+    });
+  } else {
+    jsPortfolioSpecsDivs.forEach((div, index) => {
+
+      setTimeout(() => {
+
+        div.classList.remove('active');
+      }, index * 210);
+    })
+
+    setTimeout(() => {
+
+      jsPortfolioButton.innerHTML = "Show Page Specs";
+      jsPortfolioSpecs.classList.remove('active');
+      setTimeout(() => {
+
+        jsPortfolioButton.classList.remove('active');
+      }, 200);
+    }, 800);
+  }
+});
+
+
+// Smooth Scrolling with Jquery
+$('.btn-scroll-down').on('click', function(e){
+
+  if (this.hash !== '') {
+
+    // Prevents default behaviour of click event, which would jump user to hash reference
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      // Uses this from click event to reference hash from clicked button with btn-scroll-down class
+      scrollTop: $(this.hash).offset().top - 42
+    }, 800);
+  }
 });
